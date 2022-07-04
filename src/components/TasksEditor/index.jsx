bdf90b6099d8d69c04ff01cc21e28ff25268e9ff
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2022-07-04 09:17:09 +0300

    add confetti

--- src/components/TasksEditor/index.jsx
+++ src/components/TasksEditor/index.jsx
@@ -1,40 +1,62 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faX } from "@fortawesome/free-solid-svg-icons"
 import { tasksListContext } from "../../App"
+import Confetti from 'react-confetti'
 import style from './style.module.css'
 
 export default function TasksEditor() {
+    const [numberOfPieces, setNumberOfPieces] = useState(0)
+    const [windowSize] = useState(getWindowSize());
     const { tasksList, setTasksList } = useContext(tasksListContext)
 
+    function getWindowSize() {
+        const { innerWidth, innerHeight } = window;
+        return { innerWidth, innerHeight };
+    }
+
     const deleteTask = id => {
-        let tmpTasksList = structuredClone(tasksList).filter(item => item.id != id)
+        let tmpTasksList = structuredClone(tasksList).filter(item => item.id !== id)
         setTasksList(tmpTasksList)
         localStorage.setItem('tasksList', JSON.stringify(tmpTasksList))
     }
 
-    const markAsDone = (event, id) => {
+    const markAsDone = index => {
         let tmpTasksList = structuredClone(tasksList)
+        tmpTasksList[index].isDone = !tmpTasksList[index].isDone
         setTasksList(tmpTasksList)
         localStorage.setItem('tasksList', JSON.stringify(tmpTasksList))
+        console.log(windowSize);
+        if (tmpTasksList[index].isDone) {
+            setNumberOfPieces(200)
+            setTimeout(() => setNumberOfPieces(0), 2000)
+        }
     }
 
     return (
-        <div className={style.tasksEditor}>
-            {tasksList.map((item, index) =>
-                <div className={style.singleTask} key={item.id}>
-                    <button onClick={() => deleteTask(item.id)} className={`${style.deleteBtn} ${tasksList.length == 1 ?
-                            style.justOneBtn :
-                            index == 0 ?
-                                style.lastBtn :
-                                index == tasksList.length - 1 ?
-                                    style.firstBtn
-                                    : ''
-                        }`}><FontAwesomeIcon icon={faX} /></button>
-                    <input type="checkbox" checked={item.isDone} onClick={e => markAsDone(e, item.id)}/>
-                    {item.taskName}
-                </div>
-            ).reverse()}
+        <div>
+            <Confetti
+            width={windowSize.innerWidth}
+            height={windowSize.innerHeight}
+                numberOfPieces={numberOfPieces}
+                className={style.confetti} />
+            <div className={style.tasksEditor}>
+                {tasksList.map((item, index) =>
+                    <div className={style.singleTask} key={item.id}>
+                        <button onClick={() => deleteTask(item.id)} className={`${style.deleteBtn}
+                    ${tasksList.length === 1 ?
+                                style.justOneBtn :
+                                index === 0 ?
+                                    style.lastBtn :
+                                    index === tasksList.length - 1 ?
+                                        style.firstBtn
+                                        : ''
+                            }`}><FontAwesomeIcon icon={faX} /></button>
+                        <input type="checkbox" checked={item.isDone} onClick={() => markAsDone(index)} />
+                        <div className={`${item.isDone ? style.done : ''}`}>{item.taskName}</div>
+                    </div>
+                ).reverse()}
+            </div>
         </div>
     )
 }
